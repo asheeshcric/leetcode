fd9fa96d6a248bce325e24816340771f5fbcd06c
Ashish Jaiswal <ashiz2013@gmail.com>
2021-07-05 12:45:43 -0500

    Add optimized two-sum solution using a dictionary

--- two-sum/two-sum.py
+++ two-sum/two-sum.py
@@ -4,3 +4,14 @@ class Solution:
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+                
+                
+# Optimized solution using a dictionary
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = dict()
+        for i,num in enumerate(nums):
+            if (target - num) in seen:
+                return [i, seen[target-num]]
+            else:
+                seen[num] = i
