2a6aa2004515c2f415cb2fba54cdc588df441fb2
Ashish Jaiswal <ashiz2013@gmail.com>
2021-10-12 18:47:53 -0500

    Time: 265 ms (33.26%), Space: 15.6 MB (5.62%) - LeetHub

---
+++ number-of-provinces/number-of-provinces.py
@@ -0,0 +1,41 @@
+from collections import defaultdict
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        graph = self.buildGraph(isConnected)
+        print(graph)
+        
+        def dfs(node):
+            stack = [node]
+            while stack:
+                node = stack.pop()
+                if node in visited:
+                    continue
+                for neigh in graph[node]:
+                    stack.append(neigh)
+                    
+                visited.add(node)
+        
+        # Now find the number of connected components
+        components = 0
+        visited = set()
+        for node in graph:
+            if node not in visited:
+                dfs(node)
+                components += 1
+            
+        return components
+        
+    def buildGraph(self, isConnected):
+        graph = dict()
+        for node, connections in enumerate(isConnected):
+            graph[node] = set()
+            for other_node, connection in enumerate(connections):
+                if other_node == node:
+                    continue
+                if connection == 1:
+                    # This means the node is connected to the original node
+                    graph[node].add(other_node)
+                    
+        return graph
+            
